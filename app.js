b4c14053b82cde60267ef2181c449a5a60f989b5
Vidky Adhi Pradana <pradana2766@gmail.com>
2023-12-06 06:40:30 +0700

    Belajar Set Timeout dan Interval

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log("Apakah kamu sayang aku nan");
+// setTimeout(() => {
+//     console.log("Iya dong masa engga");
+// }, 5000);
+
+// setTimeout(() => {
+//     console.log("Makasih nan");
+// }, 9000);
+
+const interval = setInterval(() => {
+    console.log(Math.random());
+}, 2000);
