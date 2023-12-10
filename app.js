69f3efa2912e8a0a67737961df3498b7b31d6c0b
Vidky Adhi Pradana <pradana2766@gmail.com>
2023-12-11 06:59:03 +0700

    Belajar reduce

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ const examScore = [80,89,70,90,93,77];
 const total = examScore.reduce((total,score) => {
     return total + score;
 });
+
+const animes = [
+    {
+        title: 'Attack on titan',
+        rating: 94,
+        year: 2022,
+    },
+    {
+        title: 'Naruto',
+        rating: 91,
+        year: 2002,
+    }
+];
+
+const bestAnime = animes.reduce((bestAnime, currAnime) => {
+    if (currAnime.rating > bestAnime.rating) {
+        return currAnime;
+    }
+    return bestAnime;
+});
