dc7b40a8dcc68939bf519f38c077b6da8d85da7f
Vidky Adhi Pradana <pradana2766@gmail.com>
2023-12-11 06:23:12 +0700

    Belajar every & some

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const examScore = [80,85,80,90,93,77];
 const isGraduate = examScore.every((score) => score >= 75);
+
+const animes = [
+    {
+        title: 'Attack on Titan',
+        rating: 87,
+        year: 2008,
+    },
+    {
+        title: 'Kemesraan',
+        rating: 85,
+        year: 2009,
+    }
+]
+
+const isAnimeListNew = animes.some((anime) => anime.year > 2010);
