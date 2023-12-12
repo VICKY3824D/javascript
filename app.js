331fbe98cbeeb55b0e6f1aac089e60ce68779ad3
Vidky Adhi Pradana <pradana2766@gmail.com>
2023-12-13 06:07:02 +0700

    Belajar this

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-const examScore = [80,89,70,90,93,77];
-
-// let total = 0;
-// for (let score of examScore) {
-//     total += score;
-// }
-
-const total = examScore.reduce((total,score) => {
-    return total + score;
-});
-
-const animes = [
-    {
-        title: 'Attack on titan',
-        rating: 94,
-        year: 2022,
-    },
-    {
-        title: 'Naruto',
-        rating: 91,
-        year: 2002,
+const person = {
+    firstName: 'Vicky',
+    lastName: 'Adhi',
+    fullName: function () {
+        return `${this.firstName} ${this.lastName}`;
     }
-];
+}
 
-const bestAnime = animes.reduce((bestAnime, currAnime) => {
-    if (currAnime.rating > bestAnime.rating) {
-        return currAnime;
-    }
-    return bestAnime;
-});
