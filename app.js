b513994b6a440a6bcc4b39f18d9d22f17f789921
Vidky Adhi Pradana <pradana2766@gmail.com>
2023-12-21 08:03:59 +0700

    Belajar Rest Parameter

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const user = {
-    nama: 'Paijo',
-    alamat: 'Tegalrejo'
+// const sumAll = (...nums) => {
+//     return nums.reduce((total,el) => total + el);
+// };
+
+const nama = ['Andi','Budi','Caca','Donny', 'Eko', 'Farhan','Gandi', 'Heru'];
+
+// rest parameter (...other)
+const pemenang = (gold, silver , bronze, ...other) => {
+    console.log(`Medali emas diraih: ${gold}`);
+    console.log(`Medali perak diraih: ${silver}`);
+    console.log(`Medali perunggu diraih ${bronze}`);
+    console.log(`Peserta lainnya: ${other}`);
 };
 
-const medsos ={
-    email: 'paijo@doe.com',
-    instagram: 'Paijo_londo'
-};
-
-//menggunakan spread operator
-const identitas = {...user,...medsos};
-
+//cara pemanggilan di console => pemenang(...nama)
