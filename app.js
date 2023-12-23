cff22ee29bed78db21e1e1eb2828fb89b6fd21c9
Vidky Adhi Pradana <pradana2766@gmail.com>
2023-12-24 05:52:14 +0700

    Belajar Unboxing object  2

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const user = {
-    name: 'Vidky',
-    email: 'pradana23@doe.com'
-}
+// const user = {
+//     name: 'Vidky',
+//     email: 'pradana23@doe.com',
+//     role: 'Admin',
+// }
 
-const {name, email, phone = '0812381928'} = user;
+// // const {name, email, phone = '0812381928'} = user;
+
+// const userAndRole = ({name, email}) => {
+//     return `${name} - ${email}`;
+// }
+
+const animes = [
+    {
+        title: 'Attack on titan',
+        rating: 94,
+        year: 2022,
+    },
+    {
+        title: 'Naruto',
+        rating: 91,
+        year: 2002,
+    }
+];
+
+const anime = animes.map(({title, year, rating}) =>{
+    return `${title} (${year}) rating: ${rating}`;
+})
